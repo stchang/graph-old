50b7e4d911b9a055f5e841047c8595e814d53073
Stephen Chang <stchang216@gmail.com>
2012-12-05 03:04:33 -0500

    split weighted and unweighted graph representation
    - split weighted and unweighted graphs into separate representations
    - add gen:graph and methods documentation

--- graph-defs.rkt
+++ graph-defs.rkt
@@ -2,9 +2,9 @@
 
 (require racket/generic)
 
-(provide (rename-out [mk-graph graph])
-         in-neighbors in-vertices)
-       ;  in-neighbors in-vertices)
+(provide gen:graph)
+(provide (rename-out [mk-graph graph] [mk-wgraph wgraph])
+         graph?)
 
 ;; ----------------------------------------------------------------------------
 ;; Generic graph interface
@@ -13,68 +13,154 @@
   (add-vertex graph node)
   (add-edge graph node1 node2 [weight])
   (add-di-edge graph from-node to-node [weight])
-  (in-vertices graph))
+  (in-vertices graph)
+  (in-neighbors graph node)
+  (in-weighted-neighbors graph node))
 
-(define DEF-WGT 1)
-(define-for-syntax DEF-WGT 1)
+;(define (graph? g) (or (G? g) (WG? g)))
 
-;; a graph G is a hash table mapping a vertex to a set of vertex-weight pairs
-;; [HashOf Vertex [SetOf (cons Vertex Weight)]]
+
+;; ----------------------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+;; Unweighted graphs
+
+;; used when weight is requested for unweighted graph
+(define DEFAULT-WEIGHT 1)
+
+;; a graph G is a 
+;;   hash table mapping vertices to sets of vertices
+;; [HashOf Vertex -> [SetOf Vertex]]
 (struct G (ht) #:transparent
   #:methods gen:graph
-  [(define (add-vertex g v) (add-vertex/internal (G-ht g) v))
-   (define (add-di-edge g u v [wgt DEF-WGT]) (add-di-edge/internal (G-ht g) u v wgt))
-   (define (add-edge g u v [wgt DEF-WGT]) (add-edge/internal (G-ht g) u v wgt))
+  [(define (add-vertex g v) (G (add-vertex/internal (G-ht g) v)))
+   (define (add-di-edge g u v [wgt 1]) (G (add-di-edge/internal (G-ht g) u v)))
+   (define (add-edge g u v [wgt 1]) (G (add-edge/internal (G-ht g) u v)))
    
-   (define (in-vertices g) (in-hash-keys (G-ht g)))])
-
-(define-syntax (in-weighted-neighbors stx)
-  (syntax-case stx ()
-    [(_ G u)
-     ;; Pos is [ListOf (cons v wgt)]
-     #'(make-do-sequence
+   (define (in-vertices g) (in-hash-keys (G-ht g)))
+   (define (in-neighbors g v)
+     (make-do-sequence
         (thunk
          (values
           ;; pos -> element
-          (match-lambda [(list (cons v wgt) rst (... ...)) (values v wgt)])
+          (match-lambda [(list-rest v _) v])
           ;; next-pos
           rest
           ;; initial pos
-          (set->list (hash-ref (G-ht G) u (set)))
+          (set->list (hash-ref (G-ht g) v (set)))
           ;; termination condition
           (λ (pos) (not (null? pos)))
           #f
-          #f)))]))
-(define-syntax (in-unweighted-neighbors stx)
-  (syntax-case stx ()
-    [(_ G u)
-     ;; Pos is [ListOf (cons v wgt)]
-     #'(make-do-sequence
+          #f))))
+   (define (in-weighted-neighbors g v)
+     (make-do-sequence
         (thunk
          (values
           ;; pos -> element
-          (match-lambda [(list (cons v wgt) rst (... ...)) v])
+          (match-lambda [(list-rest v _) (values v DEFAULT-WEIGHT)])
           ;; next-pos
           rest
           ;; initial pos
-          (set->list (hash-ref (G-ht G) u (set)))
+          (set->list (hash-ref (G-ht G) v (set)))
           ;; termination condition
           (λ (pos) (not (null? pos)))
           #f
-          #f)))]))
+          #f))))
+   ])
 
-(define-sequence-syntax in-neighbors
-  (λ () #'(λ (g u) (for/list ([(v wgt) (in-neighbors g u)]) (list v wgt))))
-  (λ (stx)
-    (syntax-case stx ()
-      [[(v wgt) (_ g u)]
-       #'[(v wgt) (in-weighted-neighbors g u)]]
-      [[v (_ g u)]
-       #'[v (in-unweighted-neighbors g u)]])))
+
+;; ----------------------------------------------------------------------------
+;; functions to manipulate unweighted graph's internal hash table 
+
+;; associate empty set with vertex v if it does not exist
+(define (add-vertex/internal ht v) (hash-update ht v (λ (s) s) (set)))
+
+  ;; add directed edge u -> v but dont try to add vertex v
+(define (add-di-edge/internal/no-vertex-check ht u v) 
+  (hash-update ht u (λ (s) (set-add s v)) (set)))
+
+;; add directed edge from u to v, and make sure vertex v is in g
+(define (add-di-edge/internal ht u v wgt)
+  (add-vertex/internal (add-di-edge/internal/no-vertex-check ht u v) v))
+
+;; add undirected edge from u to v
+(define (add-edge/internal ht u v wgt)
+  (add-di-edge/internal/no-vertex-check (add-di-edge/internal ht u v) v u))
+
 
 (define-syntax-rule (mk-graph args ...) (G (mk-internal-hash args ...)))
 
-; graph is a immutable hash table -- in the future wrap the ht with a struct
+
+;; ----------------------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+;; Weighted graphs
+
+ 
+
+;; a weighted graph WG is a 
+;;   hash table mapping vertices to sets of vertex-wgt pairs
+;; [HashOf Vertex -> [SetOf (cons Vertex Weight)]]
+(struct WG (ht) #:transparent
+  #:methods gen:graph
+  [(define (add-vertex g v) (WG (add-vertex/internal (WG-ht g) v)))
+   (define (add-di-edge g u v [wgt 0]) (WG (add-di-wedge/internal (WG-ht g) u v wgt)))
+   (define (add-edge g u v [wgt 0]) (WG (add-wedge/internal (WG-ht g) u v wgt)))
+   
+   (define (in-vertices g) (in-hash-keys (WG-ht g)))
+   (define (in-neighbors g v)
+     (make-do-sequence
+        (thunk
+         (values
+          ;; pos -> element
+          (match-lambda [(list-rest (cons v _) _) v])
+          ;; next-pos
+          rest
+          ;; initial pos
+          (set->list (hash-ref (WG-ht g) v (set)))
+          ;; termination condition
+          (λ (pos) (not (null? pos)))
+          #f
+          #f))))
+   (define (in-weighted-neighbors g v)
+     (make-do-sequence
+        (thunk
+         (values
+          ;; pos -> element
+          (match-lambda [(list-rest (cons v wgt) _) (values v wgt)])
+          ;; next-pos
+          rest
+          ;; initial pos
+          (set->list (hash-ref (WG-ht G) v (set)))
+          ;; termination condition
+          (λ (pos) (not (null? pos)))
+          #f
+          #f))))
+   ])
+
+
+;; ----------------------------------------------------------------------------
+;; functions to manipulate graph's internal hash table 
+
+;; same as unweighted
+;(define (add-vertex/internal ht v) (hash-update ht v (λ (s) s) (set)))
+
+  ;; add weighted directed edge u -> v but dont try to add vertex v
+(define (add-di-wedge/internal/no-vertex-check ht u v wgt) 
+  (hash-update ht u (λ (s) (set-add s (cons v wgt))) (set)))
+
+;; add weighted directed edge from u to v, and make sure vertex v is in g
+(define (add-di-wedge/internal ht u v wgt)
+  (add-vertex/internal (add-di-wedge/internal/no-vertex-check ht u v wgt) v))
+
+;; add weighted undirected edge from u to v
+(define (add-wedge/internal ht u v wgt)
+  (add-di-wedge/internal/no-vertex-check (add-di-wedge/internal ht u v wgt) v u wgt))
+
+
+
+(define-syntax-rule (mk-wgraph args ...) (WG (mk-internal-hash args ...)))
+
+
+;; macro to create internal hash table for both weighted and unweighted graphs
 (define-syntax (mk-internal-hash stx)
   (define (get-undirected-edge-weight e)
     (regexp-match 
@@ -112,43 +198,25 @@
      (get-undirected-edge-weight #'--wgt--)
      (let* ([wgt (string->number (car (get-undirected-edge-weight #'--wgt--)))])
        (with-syntax ([wgt (datum->syntax #'--wgt-- wgt)])
-         #'(add-edge/internal (mk-internal-hash rest ...) 'u 'v wgt)))]
+         #'(add-wedge/internal (mk-internal-hash rest ...) 'u 'v wgt)))]
     [(_ (u --wgt-> v) rest ...)
      (get-directed-edge-weight-lr #'--wgt->)
      (let* ([wgt (string->number (car (get-directed-edge-weight-lr #'--wgt->)))])
        (with-syntax ([wgt (datum->syntax #'--wgt-> wgt)])
-         #'(add-di-edge/internal (mk-internal-hash rest ...) 'u 'v wgt)))]
+         #'(add-di-wedge/internal (mk-internal-hash rest ...) 'u 'v wgt)))]
     [(_ (u <-wgt-- v) rest ...)
      (get-directed-edge-weight-rl #'<-wgt--)
      (let* ([wgt (string->number (car (get-directed-edge-weight-rl #'<-wgt--)))])
        (with-syntax ([wgt (datum->syntax #'<-wgt-- wgt)])
-         #'(add-di-edge/internal (mk-internal-hash rest ...) 'v 'u wgt)))]
+         #'(add-di-wedge/internal (mk-internal-hash rest ...) 'v 'u wgt)))]
 
     ;; literal list of pairs
     [(_ ((u ((v wgt) ...)) ...)) 
      #'(make-immutable-hash (list (cons 'u (apply set '((v . wgt) ...))) ...))]
     [(_ ((u (v ...)) ...)) 
-     #`(make-immutable-hash (list (cons 'u (apply set '((v . #,DEF-WGT) ...))) ...))]
+     #`(make-immutable-hash (list (cons 'u (apply set '(v ...))) ...))]
     
     ;; --------------------
     ;; list of pairs
     [(_ assocs) #'(make-immutable-hash assocs)]))
-
-;; ----------------------------------------------------------------------------
-;; functions to manipulate graph's internal hash table 
-
-;; associate empty set with vertex v if it does not exist
-(define (add-vertex/internal ht v) (hash-update ht v (λ (s) s) (set)))
-
-  ;; add directed edge u -> v but dont try to add vertex v
-(define (add-di-edge/internal/no-vertex-check ht u v [wgt DEF-WGT]) 
-  (hash-update ht u (λ (s) (set-add s (cons v wgt))) (set)))
-
-;; add directed edge from u to v, and make sure vertex v is in g
-(define (add-di-edge/internal ht u v [wgt DEF-WGT])
-  (add-vertex/internal (add-di-edge/internal/no-vertex-check ht u v wgt) v))
-
-;; add undirected edge from u to v
-(define (add-edge/internal ht u v [wgt DEF-WGT])
-  (add-di-edge/internal/no-vertex-check (add-di-edge/internal ht u v wgt) v u wgt))
 
--- scribblings/graph.scrbl
+++ scribblings/graph.scrbl
@@ -1,22 +1,94 @@
 #lang scribble/manual
 @(require scribble/eval
-          (for-label "../graph.rkt"
+          (for-label "../graph-defs.rkt"
                      racket/contract
                      racket/base))
 
 @title{Graphs}
 
 @(define the-eval (make-base-eval))
-@(the-eval '(require "../graph.rkt"))
+@(the-eval '(require "../graph-defs.rkt"))
 
-@defmodule["../graph.rkt"]
+@defmodule["../graph-defs.rkt"]
 
 @author[@author+email["Stephen Chang" "stchang@racket-lang.org"]]
 
 Graph library. Partially based on C++ Boost Graph Library. See Lee et al. OOPSLA 1999 @cite["GGCL"].
 
-@defproc[(graph? [v any/c]) boolean?]{
-Returns @racket[#t] if @racket[v] is a graph and @racket[#f] otherwise.}
+@defthing[gen:graph any/c]{
+  A @tech{generic interface} (see @secref["struct-generics"]) that defines a @deftech{graph}. To supply method implementations, a struct should use the @racket[#:methods] form. A @tech{graph} has the following methods:
+
+@itemize[
+ 
+  @item{@racket[add-vertex]: Accepts two arguments, a graph and a vertex. 
+         Adds the vertex to the graph.}
+  @item{@racket[add-edge]: Accepts three or four arguments, a graph, two vertices, and an optional weight value. Adds the undirected edge comprised of the two vertices and the optional weight to the graph, adding the vertices first if necessary. Equivalent to adding two directed edges, one in each direction.}
+  @item{@racket[add-di-edge]: Accepts three or four arguments, a graph, source and destination vertices, and an optional weight value. Adds to the graph the directed, optionally weighted edge going from the source to the destination.}
+  @item{@racket[in-vertices]: Accepts one argument, a graph. Returns a sequence whose elements are the vertices of the graph.}
+  @item{@racket[in-neighbors]: Accepts two arguments, a graph and a vertex. Returns a sequence whose elements are the vertex's neighbors in the graph.}
+  @item{@racket[in-weighted-neighbors]: Accepts two arguments, a graph and a vertex. Returns a sequence whose elements consist of two values, a neighbor of the vertex and the weight of the edge connecting the vertex to that neighbor.}
+          
+]
+}
+
+@defproc[(graph? [g any/c]) boolean?]{
+Returns @racket[#t] if @racket[g] is a @tech{graph} and @racket[#f] otherwise.}
+
+
+@defproc[(add-vertex [graph graph?]
+                     [v any/c])
+         graph?]{
+Adds vertex @racket[v] to @racket[graph] and returns the new graph.
+}
+                
+@defproc[(add-edge [graph graph?]
+                   [u any/c]
+                   [v any/c]
+                   [weight 1])
+         graph?]{
+
+Adds the undirected edge from @racket[u] to @racket[v], with optional 
+@racket[weight] to @racket[graph], adding the vertices first, if necessary.
+Equivalent to adding two directed edges, one in each direction.
+
+}
+                
+@defproc[(add-di-edge [graph graph?]
+                      [u any/c]
+                      [v any/c]
+                      [weight 1]) 
+         graph?]{
+
+Adds the directed edge from @racket[u] to @racket[v], with optional 
+@racket[weight] to @racket[graph], adding the vertices first, if necessary. 
+
+}
+
+@defproc[(in-vertices [graph graph?])
+         sequence?]{
+                 
+Returns a sequence whose elements are the vertices of @racket[graph].
+
+}
+
+@defproc[(in-neighbors [graph graph?]
+                       [v any/c])
+         sequence?]{
+                 
+Returns a sequence whose elements are the neighbors of vertex @racket[v] 
+in @racket[graph].
+                                                              
+}
+
+@defproc[(in-weighted-neighbors [graph graph?]
+                                [v any/c])
+         sequence?]{
+                 
+Returns a sequence whose elements consist of two values, a neighbor of
+vertex @racket[v] in @racket[graph], and the weight of the edge connecting 
+@racket[v] to that neighbor.
+
+}
 
 @defproc[(graph [assocs (listof pair?) null]) graph?]{
 Creates an immutable graph, with the given key-value pairs, where a key is a vertex in the graph and a value is the set of neighbors for that vertex}
